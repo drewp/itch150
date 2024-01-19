7d61bcfdf3c43e940d2a0b9de3f2438f2cfb4fc2
arrowira <41136800+arrowira@users.noreply.github.com>
2024-01-19 03:03:40 -0800

    procedural spell

---
+++ game/Assets/beam_spell.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+using static UnityEngine.GraphicsBuffer;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+class makebeam
+{
+    public float root, meat, flower;
+    
+    public makebeam()
+    {
+        
+        root = Random.Range(0f, 2.0f);
+        flower = Random.Range(0f, 2.0f);
+        meat = Random.Range(0f, 2.0f);
+
+       
+    }
+}
+public class beam_spell : MonoBehaviour
+{
+    public GameObject projectileObj;
+    private Rigidbody projectileRb;
+    [SerializeField]
+    private float spell_speed = 60f;
+    [SerializeField]
+    Material mat;
+    [SerializeField]
+    ParticleSystem Particles;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectileRb = projectileObj.GetComponent<Rigidbody>();
+        
+    }
+    
+
+// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            
+            makebeam spell = new makebeam();
+            Color customColor = new Color(spell.root + 0.4f, spell.meat + 0.3f, spell.flower, 1.0f);
+            mat.SetColor("_EmissionColor", customColor);
+            var main = Particles.main;
+            main.startSize = spell.root/2.2f;
+            projectileObj.transform.localScale = new Vector3(spell.meat/1.5f, spell.meat/1.5f, 0f);
+            spell_speed = 400/spell.meat/0.8f;
+
+
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 10f;
+
+            Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+            mousePos.x = mousePos.x - objectPos.x;
+            mousePos.y = mousePos.y - objectPos.y;
+
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+            Rigidbody beam;
+            beam = Instantiate(projectileRb, transform.position, Quaternion.Euler(new Vector3(0, 0, angle))) as Rigidbody;
+
+
+
+            beam.AddForce(beam.transform.right * spell_speed);
+        }
+    }
+}
---
+++ game/Assets/lifetime.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lifetime : MonoBehaviour
+{
+    [SerializeField]
+    private int time = 20;
+    int tick = 0;
+
+    void Start()
+    {
+        _tick();
+    }
+    private void _tick()
+    {
+        
+        tick++;
+        if (tick<20)
+        {
+            Debug.Log("ticked");
+            Invoke("_tick", 1f);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
