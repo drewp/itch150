dcededa8b275a529b47d569adfe2113b6bf91940
arrowira <41136800+arrowira@users.noreply.github.com>
2024-01-20 20:48:26 -0800

    UI for pickups. picking up items gets rid of them

--- game/Assets/Random Scripts/inventory.cs
+++ game/Assets/Random Scripts/inventory.cs
@@ -1,31 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class inventory : MonoBehaviour
 {
     
     public float Flower = 0f, Meat = 0f, Root = 0f;
-
+    [SerializeField]
+    private Image flowericon, rooticon, meaticon;
     
     public void add(string name, float value)
     {
         if (name == "flower" && Flower == 0f)
         {
+            flowericon.enabled = true;
             Flower= value;
             Debug.Log(Flower);
 
         }
         if (name == "meat" && Meat == 0f)
         {
+            meaticon.enabled = true;
             Meat = value;
             Debug.Log(Flower);
 
         }
         if (name == "root" && Root == 0f)
         {
+            rooticon.enabled = true;
             Root = value;
             Debug.Log(Flower);
+
+        }
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X)) 
+        { 
+            Flower = Root = Meat = 0;
+            flowericon.enabled = rooticon.enabled = meaticon.enabled = false;
+
 
         }
     }
