f0c2e55a7420ca7a1bee259d8b732499a1394c80
drewp@bigasterisk.com <drewp@bigasterisk.com>
2024-01-20 20:06:13 -0800

    cheat mode - press 'v' to fire towards mouse

--- game/Assets/Random Scripts/beam_spell.cs
+++ game/Assets/Random Scripts/beam_spell.cs
@@ -24,58 +24,66 @@ public class beam_spell : MonoBehaviour
     SpriteRenderer sr;
     [SerializeField]
     ParticleSystem Particles;
-    
 
 
-    
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        projectileRb = projectileObj.GetComponent<Rigidbody2D>();
-        
-    }
-    
 
-// Update is called once per frame
+        projectileRb = projectileObj.GetComponent<Rigidbody2D>();
+
+    }
+
+
+    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && kindling.kindle > 0 && inventory.Meat > 0 && inventory.Root > 0 && inventory.Flower > 0)
         {
 
-           
-
-
-            kindling.burn(0.05f + inventory.Meat / 8);
-
-            Color customColor = new Color(inventory.Root/2, inventory.Meat/2, inventory.Flower, 1.0f);
-            mat.SetColor("_Color", customColor);
-            sr.color = customColor;
-            var main = Particles.main;
-            main.startSize = inventory.Root/2.2f;
-            projectileObj.transform.localScale = new Vector3(inventory.Meat/1.5f, inventory.Meat/1.5f, 0f);
-            if (inventory.Meat != 0f)
-            {
-                spell_speed = inventory.Flower * 150 / inventory.Meat / 0.8f;
-            }
-            
-
-
-            Vector3 mousePos = Input.mousePosition;
-            mousePos.z = 10f;
-
-            Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
-            mousePos.x = mousePos.x - objectPos.x;
-            mousePos.y = mousePos.y - objectPos.y;
-
-            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-            Rigidbody2D beam;
-            beam = Instantiate(projectileRb, transform.position, Quaternion.Euler(new Vector3(0, 0, angle))) as Rigidbody2D;
-
-
-
-            beam.AddForce(beam.transform.right * spell_speed);
+            FireBeam();
         }
+
+        // cheat
+        if (Input.GetKeyDown("v"))
+        {
+            kindling.kindle = 99;
+            inventory.Meat = 1;
+            inventory.Root = 1;
+            inventory.Flower = 1;
+            FireBeam();
+        }
+    }
+
+    private void FireBeam()
+    {
+        kindling.burn(0.05f + inventory.Meat / 8);
+
+        Color customColor = new Color(inventory.Root / 2, inventory.Meat / 2, inventory.Flower, 1.0f);
+        mat.SetColor("_Color", customColor);
+        sr.color = customColor;
+        var main = Particles.main;
+        main.startSize = inventory.Root / 2.2f;
+        projectileObj.transform.localScale = new Vector3(inventory.Meat / 1.5f, inventory.Meat / 1.5f, 0f);
+        if (inventory.Meat != 0f)
+        {
+            spell_speed = inventory.Flower * 150 / inventory.Meat / 0.8f;
+        }
+
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10f;
+
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        Rigidbody2D beam;
+        beam = Instantiate(projectileRb, transform.position, Quaternion.Euler(new Vector3(0, 0, angle))) as Rigidbody2D;
+
+        beam.AddForce(beam.transform.right * spell_speed);
     }
 }
