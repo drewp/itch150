4591da5b7dffc54dd7c916b911d1d0a086256101
galaxyraider9wastaken <owendahms@gmail.com>
2024-01-21 19:25:29 -0800

    MADE CREDITS SCENE WORK

--- game/Assets/Random Scripts/parent.cs
+++ game/Assets/Random Scripts/parent.cs
@@ -10,6 +10,10 @@ public class parent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (GameObject.FindWithTag("Player"))
+        {
+            target = GameObject.FindWithTag("Player");
+        }
         transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
     }
 }
