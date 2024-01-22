8e46df7cad221cbfc668bab72593554137ef2fa9
galaxyraider9wastaken <owendahms@gmail.com>
2024-01-21 19:56:39 -0800

    Extra

--- game/Assets/enable.cs
+++ game/Assets/enable.cs
@@ -14,6 +14,7 @@ public class enable : MonoBehaviour
     public void Enable()
     {
         del = true;
+        gameObject.SetActive(true);
     }
     // Update is called once per frame
     void Update()
